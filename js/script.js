28e37c812725ff1c79168b18b25dccb6d391644e
lamees-hemdan <124389652+lamees-hemdan@users.noreply.github.com>
2023-05-08 17:28:36 +0000

    did js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2023 Lamees Hemdan All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Lamees Hemdan
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function doMathClicked () {
+  // This function multiplyes two numbers
+
+  // input
+  const number1 = parseInt(document.getElementById("number-1").value)
+  const number2 = parseInt(document.getElementById("number-2").value)
+
+  // process
+  let counter = 0
+  let answer = 0
+  while (counter < number2) {
+    answer = answer + number1
+    counter = counter + 1
+  }
+
+  // output
+  document.getElementById('number-entered').innerHTML = number1 + " x " + number2 + " = " + answer
+}
